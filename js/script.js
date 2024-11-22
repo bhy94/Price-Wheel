32cdaa62b5f4231cc69317a289e9aa25dc4deaed
bhy94 <2207508834@qq.com>
2024-11-22 11:15:17 +0800

    Full Screen

--- js/script.js
+++ js/script.js
@@ -3,3 +3,77 @@ console.log('Loading wheel animation...');
 function fakeRotateWheel() {
     console.log('Wheel is rotating...');
 }
+
+// 添加全屏相關功能
+document.addEventListener('DOMContentLoaded', function() {
+    const fullscreenBtn = document.getElementById('fullscreenBtn');
+    const container = document.querySelector('.container');
+
+    // 檢查是否支持全屏 API
+    function getFullscreenElement() {
+        return document.fullscreenElement ||
+               document.webkitFullscreenElement ||
+               document.mozFullScreenElement ||
+               document.msFullscreenElement;
+    }
+
+    // 進入全屏
+    function enterFullscreen(element) {
+        if(element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if(element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if(element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if(element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+
+    // 退出全屏
+    function exitFullscreen() {
+        if(document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if(document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if(document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if(document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+
+    // 處理全屏按鈕點擊
+    fullscreenBtn.addEventListener('click', function() {
+        if (!getFullscreenElement()) {
+            enterFullscreen(document.documentElement);
+            // 嘗試隱藏 Safari 工具欄
+            setTimeout(function() {
+                window.scrollTo(0, 1);
+            }, 100);
+        } else {
+            exitFullscreen();
+        }
+    });
+
+    // 監聽全屏狀態變化
+    document.addEventListener('fullscreenchange', handleFullscreenChange);
+    document.addEventListener('webkitfullscreenchange', handleFullscreenChange);
+    document.addEventListener('mozfullscreenchange', handleFullscreenChange);
+    document.addEventListener('MSFullscreenChange', handleFullscreenChange);
+
+    function handleFullscreenChange() {
+        if (getFullscreenElement()) {
+            document.body.classList.add('is-fullscreen');
+            // 強制滾動以隱藏工具欄
+            window.scrollTo(0, 1);
+        } else {
+            document.body.classList.remove('is-fullscreen');
+        }
+    }
+
+    // 處理 iOS 的特殊情況
+    if (navigator.standalone) {
+        document.body.classList.add('is-fullscreen');
+    }
+});
